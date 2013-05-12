0e7cd0a4f840b535dce1d0c3b5026002b80b558b
sangmingming <ming_1990@qq.com>
2013-05-12 20:49:17 +0800

    完成系统优化，修改一些小问题

--- phoneSafeManager/src/com/sam/safemanager/ui/CleanActivity.java
+++ phoneSafeManager/src/com/sam/safemanager/ui/CleanActivity.java
@@ -12,6 +12,7 @@ import com.sam.safemanager.engine.SDPathService;
 import com.sam.safemanager.util.FileSizeUtil;
 import com.sam.safemanager.util.TextFormater;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
@@ -25,31 +26,36 @@ import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.pm.IPackageDataObserver;
 import android.content.pm.IPackageStatsObserver;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageStats;
+import android.content.res.Resources.Theme;
 
 public class CleanActivity extends Activity implements OnClickListener{
 	private TextView tvDesc,tvCache,tvSD;
 	private LinearLayout ll;
 	private SeekBar skbProgress;
-	private Button btnClean,btnStop;
+	private Button btnClean;
 	private CheckBox ckCache,ckSD;
 	private PackageManager pm;
 	private long cacheSize = 0;
 	private long sdSize = 0;
 	private boolean hasSd = false;
 	private String sdPath;
+	private int prosd=0;
+	ProgressDialog prodialog;
 	List<PackageInfo> packageinfos;
+	private boolean cleansdfinish = true;
+	private boolean cleancachefinish = true;
 	private List<String> pathlists = new ArrayList<String>();
 	private Handler handler = new Handler(){
 		public void handleMessage(android.os.Message msg) {
 			switch(msg.what){
 			case 1:
 				tvCache.setText("程序缓存:"+TextFormater.getDataSize(cacheSize));
-				tvSD.setText("SD卡垃圾文件:"+TextFormater.getDataSize(sdSize));
 				btnClean.setVisibility(View.VISIBLE);
 				btnClean.setText("一键清理");
 				break;
@@ -76,11 +82,9 @@ public class CleanActivity extends Activity implements OnClickListener{
 		ll = (LinearLayout) findViewById(R.id.ll_clean);
 		skbProgress = (SeekBar) findViewById(R.id.skb);
 		btnClean = (Button) findViewById(R.id.btn_clean);
-		btnStop = (Button) findViewById(R.id.btn_stop);
 		ckCache = (CheckBox) findViewById(R.id.ck_cache);
 		ckSD = (CheckBox) findViewById(R.id.ck_sd);
 		btnClean.setOnClickListener(this);
-		btnStop.setOnClickListener(this);
 		ll.setVisibility(View.GONE);
 	}
 
@@ -93,6 +97,8 @@ public class CleanActivity extends Activity implements OnClickListener{
 				ll.setVisibility(View.VISIBLE);
 				btnClean.setVisibility(View.GONE);
 				findCache();
+			}else if(btnClean.getText().toString().equals("一键清理")){
+				clearAllCache();
 			}
 				
 			break;
@@ -102,15 +108,14 @@ public class CleanActivity extends Activity implements OnClickListener{
 	}
 	
 	private void findCache(){
+		sdSize = 0;
+		cacheSize = 0;
+		prosd = 0;
 			pm = getPackageManager();
 			// 1.获取所有的安装好的应用程序
 			packageinfos = pm
 						.getInstalledPackages(PackageManager.GET_UNINSTALLED_PACKAGES);
-			skbProgress.setMax(packageinfos.size()*2);
-			skbProgress.setProgress(0);
-			
 			for (PackageInfo info : packageinfos) {
-				getAppCacheSize(info.packageName);
 				if(hasSd){
 					String path = SDPathService.getsdPath(info.packageName);
 					skbProgress.setProgress(skbProgress.getProgress()+1);
@@ -121,13 +126,20 @@ public class CleanActivity extends Activity implements OnClickListener{
 						try {
 							//sdSize += FileSizeUtil.getFileSize(f);
 						} catch (Exception e) {
-							// TODO Auto-generated catch block
+							
 							e.printStackTrace();
 						}
 					}
 				}
 				
+				
 			}
+			skbProgress.setMax(packageinfos.size()+pathlists.size()*3);
+			skbProgress.setProgress(0);
+			for(PackageInfo info : packageinfos){
+				getAppCacheSize(info.packageName);
+			}
+			new FindSdCacheAsync().execute();
 	}
 	
 	/**
@@ -162,15 +174,51 @@ public class CleanActivity extends Activity implements OnClickListener{
 		
 	}
 	
+	private void clearAllCache(){
+		prodialog = new ProgressDialog(this);
+		prodialog.setMessage("正在删除缓存，请稍后。。。");
+		prodialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		prodialog.show();
+		if(ckCache.isChecked()){
+			clearCache();
+		}
+		if(ckSD.isChecked()){
+			clearSDcache();
+		}
+		if(!ckCache.isChecked() && !ckSD.isChecked())
+			prodialog.dismiss();
+	}
+	
 	
 	/**
 	 * 删除sd卡上的垃圾文件
 	 */
 	private void clearSDcache(){
-		for(String path:pathlists){
-			File p = new File(path);
-			deleteFile(p);
-		}
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				cleansdfinish = false;
+				for(String path:pathlists){
+					File p = new File(path);
+					deleteFile(p);
+				}
+				cleansdfinish = true;
+				CleanActivity.this.runOnUiThread(new Runnable() {
+					
+					@Override
+					public void run() {
+						if(cleansdfinish && cleancachefinish)
+							prodialog.dismiss();
+						
+					}
+				});
+				
+				
+			}
+		}).start();
+		
+		
 	}
 	
 	/**
@@ -191,8 +239,8 @@ public class CleanActivity extends Activity implements OnClickListener{
 		}
 	}
 	
-	private void clearCache() {  
-
+	private void clearCache() {
+		cleancachefinish = false;
         try {  
             Method localMethod = pm.getClass().getMethod("freeStorageAndNotify", Long.TYPE,  
                     IPackageDataObserver.class);  
@@ -203,6 +251,11 @@ public class CleanActivity extends Activity implements OnClickListener{
 
                 @Override  
                 public void onRemoveCompleted(String packageName, boolean succeeded) throws RemoteException { 
+                	if(packageName.equals(packageinfos.get(packageinfos.size()-1).packageName)){
+                		cleancachefinish = true;
+                		if(cleansdfinish && cleancachefinish)
+							prodialog.dismiss();
+                	}
                 }  
             });  
         } catch (Exception e) {  
@@ -224,7 +277,37 @@ public class CleanActivity extends Activity implements OnClickListener{
       }
     }
 	
-	
+	class FindSdCacheAsync extends AsyncTask<Void, Integer, Void>{
+		
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			super.onProgressUpdate(values);
+			
+			skbProgress.setProgress(skbProgress.getProgress()+(values[0]-prosd)*3);
+			prosd = values[0];
+			tvSD.setText("SD卡垃圾文件:"+TextFormater.getDataSize(sdSize));
+		}
+		
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			sdSize = 0;
+			int x=0;
+			for(String path:pathlists){
+				File f = new File(path);
+				try {
+					sdSize +=FileSizeUtil.getFileSize(f);
+					x++;
+					publishProgress(x);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			return null;
+		}
+		
+	}
 	
 
 }
